1f8d64d70f2957804408530070511c9359599f8d
lin810221 <75887784+lin810221@users.noreply.github.com>
2022-11-15 18:10:22 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,65 +1,64 @@
 import PySimpleGUI as sg
 
+sg.theme('DarkAmber') # 主題設置
 
-sg.theme('Darkblue1')
-
-# 處理登入問題
 def login():
-    ans = 0
     layout = [
         [sg.Text('USERNAME')],
         [sg.Input(key = '_USER_')],
         [sg.Text('PASSWORD')],
         [sg.Input(key = '_PWD_', password_char = '*')],
-        [sg.Ok('Enter', button_color='#000000', size = (40, 1))]]
-    
-    window = sg.Window(title="Login", layout = layout, 
-                       element_justification='c', return_keyboard_events=True)
+        [sg.Ok('Enter', size = (40, 1))]]
 
-    while True:
-        event, values = window.read()
-        
-        if event == 'Enter' or event == 'Enter:13':
-            if values['_USER_'] == 'admin' and values['_PWD_'] == '0000':
-                ans += 1
-                sg.popup('Success!')
-                break
-            else:
-                sg.popup('Failed!', text_color = 'red')
-        
-        if event == sg.WINDOW_CLOSED:
-            break
-    
-    window.close()
-    return ans
+    return sg.Window(title = "Login", layout = layout, 
+                     finalize=True,  element_justification='c', 
+                     return_keyboard_events=True)
 
-# 登入條
 def loading():
-    layout = [
-        [sg.Text('Loading')],
-        [sg.ProgressBar(10000, orientation='h', size=(20, 20), key='_progressBar_')],]
+    layout = [[sg.Text('Loading')],
+              [sg.ProgressBar(3000, orientation='h', size=(20, 20), key='_progressBar_')],]
 
     window = sg.Window(title = 'Loading', layout = layout)
 
     while True:
-        event, values = window.read(timeout = 1000)
+        event, values = window.read(timeout = 0)
         progress_bar = window['_progressBar_']
-        for i in range(10000):
+        for i in range(3000):
             progress_bar.UpdateBar(i + 1)
         break
 
-        
         if event == sg.WINDOW_CLOSED:
             break
 
     window.close()
 
-# 主功能
-def main():
-    login_result = login()
-    if login_result == 1:
-        loading()
 
+def make_win1():
+    layout = [[sg.Text('void'), sg.Text('      ', k='-OUTPUT-')]]
+    
+    return sg.Window('Window Title', layout, finalize=True)
+
+
+def main():
+    window1, window2 = login(), None
+    
+    while True:
+        window, event, values = sg.read_all_windows()
+        if event == sg.WINDOW_CLOSED:
+                break
+            
+        if window == window1:
+            if event == 'Enter' or event == 'Enter:13':
+                if values['_USER_'] == 'a' and values['_PWD_'] == 'a':
+                    sg.popup('Success!')
+                    window1.Close()
+                    loading()
+                    window2 = make_win1()
+                    
+                else:
+                    sg.popup('Failed!', text_color = 'red')
+    
+    window.close()
 
 if __name__ == '__main__':
     main()
