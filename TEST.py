caf027d79ed6916ea384861f375143bfe60e46a8
Ian <75887784+lin810221@users.noreply.github.com>
2023-10-30 11:30:03 +0800

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,43 @@
+import os
+
+
+#folder_path = input("Input folder PATH: ")
+
+folder_path = 'D:\RENAME'
+
+def get_files_name(folder_path):
+    try:
+        # Get all files & folders in the directory
+        items = os.listdir(folder_path)
+    
+        # Filter out folders and kepp only file names
+        files = [item for item in items if os.path.isfile(os.path.join(folder_path, item))]
+
+        return files
+    
+    except FileNotFoundError:
+        print("Folder not found.")
+    
+    except PermissionError:
+        print("Permission denied to access the folder.")
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+files = get_files_name(folder_path)
+
+otp_files = []
+flash_files = []
+
+for file_name in files:
+    if "otp" in file_name.lower():
+        otp_files.append(file_name)
+    
+    elif "flash" in file_name.lower():
+        flash_files.append(file_name)
+
+
+for file_name in files:
+    base, extension = os.path.splitext(file_name)
+    print(f'BASE: {base}')
+    print(f'EXT: {extension}\n')
